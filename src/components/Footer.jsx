84edffed34a4393ad4328f774c7e1dc25ceef9db
Kaveesha Sulakshana <kaveeshajima2004@gmail.com>
2025-12-07 09:53:35 +0530

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-6 bg-gray-900 text-gray-400 text-center">
-      <p>© {new Date().getFullYear()} Kaveesha Sulakshana. All Rights Reserved.</p>
-    </footer>
-  );
-}
